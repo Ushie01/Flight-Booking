60fffe90907f039617879fa59a8eb1c0713ed27f
Ushie01 <abugodwinaj@gmail.com>
2023-05-25 12:04:04 +0100

    create:Three way toggle component[master]

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,18 @@
+import { GenericBurgerRegular } from '@heathmont/moon-icons-tw';
+import { ToggleNavbar } from '@/src/client/components/ToggleNav';
+
+
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className='flex flex-col items-center justify-center p-4 bg-gray-100'>
+      <nav className="flex flex-row items-center justify-between w-full">
+        <div></div>
+        <p className="text-2xl font-bold ml-10">Book Flight</p>
+        <GenericBurgerRegular width={40} height={40} color='black' />
+      </nav>
+      <ToggleNavbar />
+    </div>
+  );
+};
 
 export default Home;
